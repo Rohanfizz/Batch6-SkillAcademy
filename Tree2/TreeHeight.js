913c9b40069cc49494c88f4969dc643e788c23a4
Rohanfizz <rohans0501@gmail.com>
2023-04-16 21:58:43 +0530

    tree2

---
+++ Tree2/TreeHeight.js
@@ -0,0 +1,45 @@
+// let tree = [20, 8, 4, -1, -1, 12, 10, -1, -1, 14, -1, -1, -1];
+class Node {
+    left;
+    right;
+    val;
+    constructor(v) {
+        this.val = v;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function serialize(tree) {
+    let idx = 0;
+    function solve(tree) {
+        if (idx == tree.length) return null;
+        if (tree[idx] == -1) {
+            idx++;
+            return null;
+        }
+        let nn = new Node(tree[idx]);
+        idx++;
+        nn.left = solve(tree);
+        nn.right = solve(tree);
+        return nn;
+    }
+
+    let root = solve(tree);
+    return root;
+}
+
+// let tree = [1,3,4,-1,-1,2,8,-1,-1,-1,5,6,-1,9,-1,-1,7,-1,-1];
+let tree = [5,10,3,-1,-1,4,-1,8,-1,-1,12,6,-1,-1,7,-1,-1];
+let root = serialize(tree);
+
+function getHeight(node){
+    if(node == null) return 0;
+
+    let leftHeight = getHeight(node.left);
+    let rightHeight = getHeight(node.right);
+
+    return Math.max(leftHeight,rightHeight)+1;
+}
+
+console.log(getHeight(root));
---
+++ Tree2/treeContruct.js
@@ -0,0 +1,49 @@
+class Node{
+    left;
+    right;
+    val;
+    constructor(data){
+        this.val = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+let idx = 0;
+function serialize(arr){//this function return node always
+    if(arr[idx] == -1){
+        idx++;
+        return null;
+    }
+
+    let me = new Node(arr[idx]);
+    idx++;
+
+    let leftChild = serialize(arr);
+    let rightChild = serialize(arr);
+
+    me.left= leftChild;
+    me.right = rightChild;
+    return me;
+}
+
+let tree = [70,2,4,-1,6,-1,-1,-1,3,-1,5,7,-1,-1,-1];
+let root = serialize(tree);
+
+displayTree(root);
+function displayTree(node){
+    if(node  == null) return;
+
+    let s = "";
+    s+= node.val +" : ";
+    if(node.left == null) s+="-1, ";
+    else s+=node.left.val+", ";
+
+    if(node.right == null) s+="-1";
+    else s+=node.right.val;
+
+    console.log(s);
+
+    displayTree(node.left);
+    displayTree(node.right);
+}
